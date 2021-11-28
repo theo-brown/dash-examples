bde0841ca2e7307d98f29f4f5d93de056a042d51
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-11-28 20:24:33 +0000

    Use deque for extending data

--- realtime-plot.py
+++ realtime-plot.py
@@ -4,34 +4,40 @@ from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 import numpy as np
-import pandas as pd
+from collections import deque
 
+NUM_POINTS = None
+REFRESH_RATE_MS = 50
+
+x_data = deque(maxlen=NUM_POINTS)
+y_data = deque(maxlen=NUM_POINTS)
 
 app = dash.Dash(__name__,
                 external_stylesheets=['static/main.css'])
 
-figure1 = go.Figure()
-figure1.add_scatter(name='gaussian_data')
-
-data = pd.DataFrame()
-
 app.layout = html.Div([
     html.Div([
         html.H1("Realtime display of Gaussian data"),
-        dcc.Graph(id='live-update-graph1', figure=figure1),
+        dcc.Graph(id='live-update-graph1',
+                  animate=False,
+                  config={'displayModeBar': False,
+                          'staticPlot': True}),
         dcc.Interval(id='update-timer1',
-                     interval=500)  # interval in ms
+                     interval=REFRESH_RATE_MS)
     ], className="container vcontainer content")
 ], className="container vcontainer page")
 
 
 @app.callback(Output('live-update-graph1', 'figure'),
               Input('update-timer1', 'n_intervals'))
-def update_graph(n_intervals: int) -> go.Figure:
-    figure1.update_traces(x=[n_intervals],
-                          y=[np.random.normal()],
-                          selector={'name': 'gaussian_data'})
-    return figure1
+def update_graph(n_intervals: int) -> dict:
+    x_data.append(x_data[-1] + 1 if x_data else 0)
+    y_data.append(np.random.normal())
+
+    return {'data': [go.Scatter(x=list(x_data),
+                                y=list(y_data),
+                                mode='lines')]}
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
